9695da2552af7e48df8f36271a805823fe83da97
Lockaa <143623393+Lockaa@users.noreply.github.com>
2024-07-23 09:55:07 -0400

    fixed-405

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,21 @@
-var jsonArray = ["a"];
-jsonArray.pop();
+var jsonArray = [];
+var submitBtn = document.getElementById("myForm");
 
 if(localStorage.getItem("data")) {
-  jsonArray = jsonArray.concat(JSON.parse(localStorage.getItem("data")));
+  jsonArray = JSON.parse(localStorage.getItem("data"));
 }
 
-localStorage.clear();
-
-
-document.getElementById('myForm').onsubmit = function() {
-  localStorage.clear();
-  var name = document.getElementById('name').value;
-  var blogTitle = document.getElementById('blog-title').value;
-  var blogContent = document.getElementById('content').value;
+function doForm(event) {
+  event.preventDefault();
+  var name = document.getElementById("name").value;
+  var blogTitle = document.getElementById("blog-title").value;
+  var blogContent = document.getElementById("content").value;
 
   var data = {"name": name, "title": blogTitle, "content": blogContent};
-
   jsonArray.push(data);
-
   localStorage.setItem("data", JSON.stringify(jsonArray));
-}
+
+  window.location.href = "./blog.html";
+};
+
+submitBtn.addEventListener("submit", doForm);
